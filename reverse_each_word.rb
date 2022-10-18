e1b25618e341b1aa3c1bcdc4040be509c37c735d
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-19 01:23:38 +0300

    This commit adds the code that can reverse each word in a sentence string

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,6 @@
-# Write your code here
+def reverse_each_word(sentence)
+    puts sentence
+    sentence.split.map(&:reverse).join(" ")
+  end
+
+  puts reverse_each_word('hello there, and how are you?')
